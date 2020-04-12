04358751ac0a58bd031e8448a9533666155ae976
Amanjeet Gilard <amanjeet.gilard@soroco.com>
2020-04-12 17:30:41 +0530

    overview added and color for even row

--- src/components/movieTable.js
+++ src/components/movieTable.js
@@ -7,21 +7,21 @@ const Moviestable = (props) => {
             <table>
                 <tbody>
                     <tr>
-                    <th>Title</th>
-                    <th>Language</th>
-                    <th>Voters Average</th>
-                </tr>
-                {
-                    movies.map((movie)=>{
-                        return(
-                            <tr key={movie.id}>
-                                <td title={movie.original_title}>{movie.original_title}</td>
-                                <td title={movie.original_language}>{movie.original_language}</td>
-                                <td title={movie.vote_average}>{movie.vote_average}</td>
-                            </tr>
-                        )
-                    })
-                }
+                        <th>Title</th>
+                        <th>Language</th>
+                        <th>Voters Average</th>
+                    </tr>
+                    {
+                        movies.map((movie) => {
+                            return (
+                                <tr key={movie.id} title={movie.overview}>
+                                    <td >{movie.original_title}</td>
+                                    <td>{movie.original_language}</td>
+                                    <td >{movie.vote_average}</td>
+                                </tr>
+                            )
+                        })
+                    }
                 </tbody>
 
             </table>
